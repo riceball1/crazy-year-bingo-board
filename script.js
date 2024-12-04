947dd9c23063888d9027e5510a922b3a0f43cc2f
danang <ngdana1@gmail.com>
2024-12-03 22:52:40 -0800

    update bingo site

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Button event listener
+    const button = document.getElementById('generate-button')
+    button.addEventListener('click', () => {
+        alert('Button clicked!')
+    })
+
+});
